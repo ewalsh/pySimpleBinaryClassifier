1ab1c041ebe64aae5968158bcdf78f029d96eb78
Edmund Walsh <ewalsh@economicdatasciences.com>
2022-08-31 15:21:38 +0100

    obviously can't use match

--- app.py
+++ app.py
@@ -5,18 +5,17 @@ import sys
 
 if __name__ == "__main__":
     # check version
-    match sys.version.find('3.10'):
-        case 0:
-            # initial EDA
-            eda = EDA()
-            # loop over variables
-            for nm in eda.col_nms:
-                eda.gen_scatter(nm)
-                eda.plot_dist(nm)
+    if sys.version.find('3.10') == 0 :
+        # initial EDA
+        eda = EDA()
+        # loop over variables
+        for nm in eda.col_nms:
+            eda.gen_scatter(nm)
+            eda.plot_dist(nm)
 
-            # assemble the feature data sets
-            assemble_features(eda.train_data)
+        # assemble the feature data sets
+        assemble_features(eda.train_data)
 
-            spark.sql("SELECT * FROM train").show()
-        case other:
-            print('Please install python 3.10')
+        spark.sql("SELECT * FROM train").show()
+    else:
+        print('Please install python 3.10')
