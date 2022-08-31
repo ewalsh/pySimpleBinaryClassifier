ab1fadb7b86352de988c8eb15db39129d2894d6c
Edmund Walsh <ewalsh@economicdatasciences.com>
2022-08-31 20:04:45 +0100

    adding svm

--- simp_binary_classify/model.py
+++ simp_binary_classify/model.py
@@ -18,8 +18,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def sparse_to_labeledpt(label, features):
+    tmp = [0 for x in range(x.features.size)]
+    counter = 0
+    for i in x.features.indices:
+            tmp[i] = x.features.values[counter]
+            counter = counter + 1
+    return(LabeledPoint(float(x.label), tmp))
+
+def features_to_list(features):
+    tmp = [0 for x in range(x.features.size)]
+    counter = 0
+    for i in x.features.indices:
+            tmp[i] = x.features.values[counter]
+            counter = counter + 1
+    return(tmp)
+
 def gen_model(model_train: pyspark.sql.DataFrame, model_name: String, num_splits: int = 3):
     nfolds = float(os.getenv("nfolds", "10"))
+    tv_split = float(os.getenv("test_validate_split", "0.75"))
     match model_name:
         case "logistic":
             lr = LogisticRegression()
@@ -37,7 +54,7 @@ def gen_model(model_train: pyspark.sql.DataFrame, model_name: String, num_splits
             #                            parallelism=cpu_count(),\
             #                            seed=29)
             #
-            cnt = model_train.count() * 0.75
+            cnt = model_train.count() * tv_split
             crossval = CrossValidator(estimator=pipeline,
                                       estimatorParamMaps=paramGrid,
                                       evaluator=BinaryClassificationEvaluator(),
@@ -45,14 +62,28 @@ def gen_model(model_train: pyspark.sql.DataFrame, model_name: String, num_splits
 
             # for each num_split, randomly split data to create
             # a validation subgroup
+            mod_list = []
+            mse_list = []
             for i in range(num_splits):
                 sub_sample_DF = spark.sql("SELECT * FROM model_train TABLESAMPLE (75 PERCENT)")
                 sub_sample_DF.createOrReplaceTempView("sub")
                 tgt_sample_DF = spark.sql("SELECT * FROM model_train WHERE id NOT IN (SELECT id FROM sub)")
                 cvModel = crossval.fit(sub_sample_DF)
+                sub_test = cvModel.transform(tgt_sample_DF)
+                sub_test.createOrReplaceTempView("sub_test")
+                oos_mse = spark.sql("SELECT SUM(pow(label - prediction, 2)) as mse FROM sub_test").collect()
+                mse_list.append(oos_mse)
+                mod_list.append(cvModel)
         case "svm":
-            model_train_rdd = model_train.rdd.map(lambda x: LabeledPoint(float(x.label), x.features.values))
-
+            mod_list = []
+            mse_list = []
+            for i in range(num_splits):
+                sub_sample_DF = spark.sql("SELECT * FROM model_train TABLESAMPLE (75 PERCENT)")
+                sub_sample_DF.createOrReplaceTempView("sub")
+                tgt_sample_DF = spark.sql("SELECT * FROM model_train WHERE id NOT IN (SELECT id FROM sub)")
+                sub_model_train_rdd = sub_sample_DF.rdd.map(lambda x: sparse_to_labeledpt(x.label, x.features))
+                svm = SVMWithSGD.train(sub_model_train_rdd, iterations=nfolds)
+                tgt_rdd = tgt_sample_DF.rdd.map(lambda x: features_to_list(x.features))
     # model_train = spark.sql("SELECT * FROM model_train")
     # tvsModel = tvs.fit(model_train)
     # tvsModel.getTrainRatio()
