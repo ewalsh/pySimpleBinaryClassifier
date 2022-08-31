9f0d84dd1ce8db57708dae268307032e7fca96bc
Edmund Walsh <ewalsh@economicdatasciences.com>
2022-08-31 15:20:00 +0100

    ask for python 3.10

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from simp_binary_classify.eda import EDA
 from simp_binary_classify.spark import spark
 from simp_binary_classify.assembler import assemble_features
+import sys
 
 if __name__ == "__main__":
-    # initial EDA
-    eda = EDA()
-    # loop over variables
-    for nm in eda.col_nms:
-        eda.gen_scatter(nm)
-        eda.plot_dist(nm)
+    # check version
+    match sys.version.find('3.10'):
+        case 0:
+            # initial EDA
+            eda = EDA()
+            # loop over variables
+            for nm in eda.col_nms:
+                eda.gen_scatter(nm)
+                eda.plot_dist(nm)
 
-    # assemble the feature data sets
-    assemble_features(eda.train_data)
+            # assemble the feature data sets
+            assemble_features(eda.train_data)
 
-    spark.sql("SELECT * FROM train").show()
+            spark.sql("SELECT * FROM train").show()
+        case other:
+            print('Please install python 3.10')
